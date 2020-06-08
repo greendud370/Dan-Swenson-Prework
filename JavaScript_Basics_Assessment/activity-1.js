319f2aac135405b6d03ec48b52c91fac4ff88235
unknown <swenso7@stolaf.edu>
2020-06-08 16:18:28 -0500

    start Mod-2 Assess

--- JavaScript_Basics_Assessment/activity-1.js
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -1,18 +1,23 @@
 //1 -- product of 9 and 9 
 let q1 = 9+9;
+console.log(q1);
 
 //2 -- quotient of 56 and 12
 let q2 = 56/12;
+console.log(q2);
 
 //3 -- remainder of 281 divided by 9
 let q3 = 281%9;
+console.log(q3);
 
 //4 -- 18 divided by 0
 let q4 = 18/0;
+console.log(q4);
 
 //5 -- sum of string 56 and string 92
 let q5 = '56' + '92';
+console.log(q5);
 
 //6 -- any comparative operators with 42 and 52 such that it returns false
 let q6 = 42 > 52;
-
+console.log(q6);
